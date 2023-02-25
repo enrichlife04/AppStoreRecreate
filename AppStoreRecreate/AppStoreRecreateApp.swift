5c9dfbf6c108140c73e28a968fbc473af5d63e59
enrichlife04 <enrichlife0429@gmail.com>
2023-02-25 18:22:15 +0900

    Initial Commit

---
+++ AppStoreRecreate/AppStoreRecreateApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreRecreateApp.swift
+//  AppStoreRecreate
+//
+//  Created by 村中光一 on 2023/02/25.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreRecreateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
